3bb35aa2744b798439c0761279494ee9a9f50095
Adam Walker <adam@perk.gg>
2025-11-16 11:09:11 +0000

    Remove commented-out tool recipe handling logic and clean up `RecipeManagement`.

--- src/main/kotlin/org/shotrush/atom/content/RecipeManagement.kt
+++ src/main/kotlin/org/shotrush/atom/content/RecipeManagement.kt
@@ -1,33 +1,11 @@
 package org.shotrush.atom.content
 
-import org.bukkit.NamespacedKey
 import org.bukkit.event.Listener
 import org.shotrush.atom.Atom
 
 object RecipeManagement : Listener {
     fun handle(atom: Atom) {
         val server = atom.server
-//        val toolMaterials = listOf(
-//            "wood",
-//            "stone",
-//            "iron",
-//            "gold",
-//            "diamond",
-//            "netherite"
-//        )
-//        val tools = listOf(
-//            "shovel",
-//            "pickaxe",
-//            "axe",
-//            "hoe",
-//            "sword"
-//        )
-//        tools.forEach { tool ->
-//            toolMaterials.forEach { material ->
-//                server.removeRecipe(NamespacedKey("minecraft", "${material}_$tool"))
-//            }
-//        }
-
         server.clearRecipes()
     }
 }
